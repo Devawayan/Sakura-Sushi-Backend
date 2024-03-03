69bc1170b050c4760aef0b0e7e5aaa8d7a282ee4
Devawayan <devawayanhore92460@gmail.com>
2024-03-04 02:45:56 +0530

    Sakura Sushi

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -17,3 +17,16 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("Country is required and must be a string"),
     handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").notEmpty().withMessage("Restaurant Name is Required"),
+    body("city").notEmpty().withMessage("City Name is Required"),
+    body("country").notEmpty().withMessage("Country Name is Required"),
+    body("deliveryPrice").isFloat({ min: 0 }).withMessage("Delivery price must be a positive Number"),
+    body("estimatedDeliveryTime").isInt({ min: 0 }).withMessage("Estimated delivery time must be a positive number"),
+    body("cuisines").isArray().withMessage("Cuisines must be an set of Numbers together").not().isEmpty().withMessage("Cuisines cannot be Empty it needs to be specified"),
+    body("menuItems").isArray().withMessage("Menu Items must also be set of an Array"),
+    body("menuItems.*.name").notEmpty().withMessage("Meu Items name is Required"),
+    body("menuItems.*.price").isFloat({ min: 0 }).withMessage("Meu Items price is Required and must be an positive number"),
+    handleValidationErrors,
+];
