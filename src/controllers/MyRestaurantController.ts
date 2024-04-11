1acdbd096a310e86b745c28532a426466e55dff2
Devawayan <devawayanhore92460@gmail.com>
2024-04-11 17:30:42 +0530

    Sakura Sushi

--- src/controllers/MyRestaurantController.ts
+++ src/controllers/MyRestaurantController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
 import cloudinary from "cloudinary";
 import mongoose from "mongoose";
+import Order from "../models/order";
 
 const getMyRestaurant = async (req: Request, res: Response) => {
     try {
@@ -77,6 +78,53 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
     }
 };
 
+const getMyRestaurantOrders = async (req: Request, res:Response) => {
+    try {
+        const restaurant = await Restaurant.findOne({user: req.userId});
+        if(!restaurant){
+            return res.status(404).json({ message: "Restaurant Not Found" })
+        }
+
+        const orders = await Order.find({ restaurant: restaurant._id })
+            .populate("restaurant")
+            .populate("user");
+
+        res.json(orders);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something Went Wrong" })
+    }
+}
+
+const updateOrderStatus = async (req: Request, res: Response) => {
+    try {
+        const { orderId } = req.params;
+        const { status } = req.body;
+
+        const order = await Order.findById(orderId);
+
+        if(!order){
+            return res.status(404).json({ message: "Order not Found" });
+        }
+
+        const restaurant = await Restaurant.findById(order.restaurant)
+
+        if(restaurant?.user?._id.toString() !== req.userId){
+            return res.status(401).send();
+        }
+
+        order.status = status;
+        await order.save();
+
+        res.status(200).json(order);
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Unable to Update Order Status" });
+    }
+}
+
 const uploadImage = async (file: Express.Multer.File) => {
     const image = file;
     const base64Image = Buffer.from(image.buffer).toString("base64");
@@ -88,6 +136,8 @@ const uploadImage = async (file: Express.Multer.File) => {
 };
 
 export default {
+    updateOrderStatus,
+    getMyRestaurantOrders,
     createMyRestaurant,
     getMyRestaurant,
     updateMyRestaurant,
